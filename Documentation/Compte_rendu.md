23ff890e4009d4f53053f228c064df2dd957a699
EnzoPss <enzopousse.06@gmail.com>
2025-10-10 17:12:42 +0200

    début création/remplissage tables

---
+++ Documentation/Compte_rendu.md
@@ -0,0 +1,15 @@
+# Compte Rendu du projet KotlinMonster
+
+### Sprint1
+
+...
+
+
+### Sprint3
+
+création table entraineur
+![img.png](img.png)
+
+diagramme ERD
+![ERD TODO.png](ERD%20TODO.png)
+
--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -3,3 +3,51 @@ CREATE TABLE Entraineurs(
     nom VARCHAR(255),
     argents INTEGER);
 
+
+CREATE TABLE EspeceMonstre (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom CHAR(255),
+    type CHAR(255),
+    baseAttaque INT,
+    baseDefense INT,
+    baseVitesse INT,
+    baseAttaqueSpe INT,
+    baseDefenseSpe INT,
+    basePv INT,
+    modAttaque DOUBLE,
+    modDefense DOUBLE,
+    modVitesse DOUBLE,
+    modAttaqueSpe DOUBLE,
+    modDefenseSpe DOUBLE,
+    modPv DOUBLE,
+    description TEXT,
+    particularites TEXT,
+    caracteres TEXT
+);
+
+
+CREATE TABLE IndividuMonstre (
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     nom CHAR(255),
+     niveau INT,
+     attaque INT,
+     defense INT,
+     vitesse INT,
+     attaqueSpe INT,
+     defenseSpe INT,
+     pvMax INT,
+     potentiel DOUBLE,
+     exp DOUBLE,
+     pv INT,
+
+    espece_id INT DEFAULT NULL REFERENCES EspeceMonstre(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    entraineur_equipe_id INT DEFAULT NULL REFERENCES Entraineurs(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    entraineur_boite_id INT DEFAULT NULL REFERENCES Entraineurs(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+
+);
